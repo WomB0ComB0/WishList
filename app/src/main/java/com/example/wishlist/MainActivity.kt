e13ea51a59c243af035f762f27a8bcf03af8e755
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2024-03-07 09:01:12 -0500

    main features done

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.wishlist
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.toSpannable
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +41,46 @@ class MainActivity : AppCompatActivity() {
         val adapter = WishListAdapter(items)
         Rv.adapter = adapter
         Rv.layoutManager = LinearLayoutManager(this)
+
+        fun clicableLink(longText: String) {
+            try {
+                print("Clicked function")
+                val spanned = SpannableString(longText)
+                val matcher = Patterns.WEB_URL.matcher(longText)
+                var matchStart: Int
+                var matchEnd: Int
+
+                while (matcher.find()) {
+                    matchStart = matcher.start()
+                    matchEnd = matcher.end()
+
+                    var url = longText.substring(matchStart, matchEnd)
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        url = "https://$url"
+                    }
+
+                    val clickableSpan: ClickableSpan = object : ClickableSpan() {
+                        override fun onClick(widget: View) {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                            startActivity(intent)
+                        }
+
+                        override fun updateDrawState(ds: TextPaint) {
+                            super.updateDrawState(ds)
+                            ds.color = Color.BLUE
+                            ds.isUnderlineText = true
+                        }
+                    }
+                    spanned.setSpan(clickableSpan, matchStart, matchEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+                itemURL.text = spanned.toString() as Editable
+                println(itemURL.text.javaClass)
+                itemURL.movementMethod = LinkMovementMethod.getInstance()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
 
         button.setOnClickListener {
             val name = itemName.text.toString()
@@ -50,5 +103,6 @@ class MainActivity : AppCompatActivity() {
                 adapter.notifyItemRemoved(position)
             }
         })
+        clicableLink(itemURL.text.toString())
     }
 }
